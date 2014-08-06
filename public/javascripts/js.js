a0d202fceb57e040a73a197798f12561855bb975
Adam Liechty <adam@doubledutch.me>
2014-08-05 18:52:02 -0700

    @doubledutch

--- public/javascripts/js.js
+++ public/javascripts/js.js
@@ -81,7 +81,7 @@ function doubledutch() {
             cycle++;
             if (end) {
                 clearInterval(iv);
-                var email = prompt("Nice! We're always looking for talented and creative engineers. Give us your email if you'd like to send us your solution and get more info. No spam lists, we promise.")
+                var email = prompt("Nice! At DoubleDutch, we're always looking for talented and creative engineers. Give us your email if you'd like to send us your solution and get more info. No spam lists, we promise.")
                 $.ajax("#", {type: "POST", contentType: "application/json", data: JSON.stringify({ "email": email, "solution": $("#code").val() })});
                 window.location = "http://doubledutch.me/about.html";
             }
